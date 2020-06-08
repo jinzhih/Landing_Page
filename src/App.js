739857c5905931eacc87c4a1c4c077012062a417
jinzhih <62887197+jinzhih@users.noreply.github.com>
2020-06-08 22:32:30 +1000

    delete logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'; import
 // import
 // 'mdbreact/dist/css/mdb.css';
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Routes from './routes/Routes';
 import '../src/assets/css/responsive.css';
